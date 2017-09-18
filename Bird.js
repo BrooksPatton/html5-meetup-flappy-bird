2421fff4fb7bd873dfc590632cff944637a88b93
Brooks Patton <brooks@brookzerker.com>
2017-09-18 16:23:30 -0600

    the bird falls down

--- Bird.js
+++ Bird.js
@@ -1,6 +1,8 @@
 class Bird {
   constructor(location) {
     this.location = location;
+    this.acceleration = createVector(0, 0);
+    this.velocity = createVector(0, 0);
 
     this.color = {r: 255, g: 0, b: 0};
     this.size = 10;
@@ -10,5 +12,15 @@ class Bird {
     noStroke();
     fill(this.color.r, this.color.g, this.color.b);
     rect(this.location.x, this.location.y, this.size, this.size);
+  }
+
+  applyForce(force) {
+    this.acceleration.add(force);
+  }
+
+  update() {
+    this.velocity.add(this.acceleration);
+    this.location.add(this.velocity);
+    this.acceleration.mult(0);
   }
 }
--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 let bird;
+let gravity;
 
 function setup() {
   createCanvas(600, 600);
 
   bird = new Bird(createVector(10, 10));
+
+  gravity = createVector(0, 0.5);
 }
 
 function draw() {
   background(0);
 
+  bird.applyForce(gravity);
+  bird.update();
   bird.display();
 }
